f19f6adeea3430efdade6d412373f900cca52066
xzdbd <xzdbd@sina.com>
2017-01-06 16:18:01 +0800

    add header Access-Control-Allow-Origin

--- routers/router.go
+++ routers/router.go
@@ -1,11 +1,17 @@
 package routers
 
 import (
+	"github.com/astaxie/beego/context"
+
 	"github.com/astaxie/beego"
 	"github.com/xzdbd/static-host/controllers"
 )
 
 func init() {
+	beego.InsertFilter("/*", beego.BeforeStatic, func(ctx *context.Context) {
+		ctx.Output.Header("Access-Control-Allow-Origin", "*")
+	})
+
 	beego.SetStaticPath("/arcgis", "static/arcgis")
 
 	beego.Router("/", &controllers.MainController{})
